eee34be67376cf162ca9c907e694030e4d9def1c
lneira19 <lneira@itba.edu.ar>
2025-05-25 22:12:35 -0300

    Finished selectors functionality.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 import {getCollectionOfRandomEvents} from "./backend/randomevents.js"
 import {CustomSelector} from "./backend/selector.js"
 import {getArrayObjectsKeyEvents, getArrayRacersObjects, getArrayTeamsObjects, getArrayTypeEventsObjects} from "./backend/staticdata.js"
+import { getNextStage } from "./backend/staticdata.js"
 
 console.log(getCollectionOfRandomEvents(5*5))
 
@@ -41,6 +42,7 @@ btn_reload.addEventListener("click", function(event) {
     updateTabMainMatrix(5, 5);
 });
 
+/* ################# CÓDIGO DEDICADO A SELECTORES ################# */
 
 // Datos para los selectores
 const eventsData = getArrayObjectsKeyEvents()
@@ -48,33 +50,397 @@ const racersData = getArrayRacersObjects()
 const teamsData = getArrayTeamsObjects()
 
 // Variables globales para los selectores
-let selectorEvents;
+let selectorEvents
+
+let selector1 = document.getElementById("selector1");
+let selector2 = document.getElementById("selector2");
+let selector3 = document.getElementById("selector3");
+let selector4 = document.getElementById("selector4");
+let selector5 = document.getElementById("selector5");
+
+let edit_box_confirm_btn_area = document.getElementById("edit_box_confirm_btn_area")
 
 // Inicializar selectores cuando el DOM esté listo
 document.addEventListener('DOMContentLoaded', function() {
     // Crear selector de tecnologías
+    
     selectorEvents = new CustomSelector('selector1', eventsData, {
         label: 'Select the event type:',
         placeholder: 'Choose an event type of your liking',
         onSelectionChange: function(selection) {
+            
+            edit_box_confirm_btn_area.innerHTML = ""; // Limpiar el área del botón antes de agregar uno nuevo
+            
+            // Creación de botón de confirmación
+            const btn_edit_box_confirm = document.createElement("button");
+            btn_edit_box_confirm.id = "btn_edit_box_confirm";
+            btn_edit_box_confirm.textContent = "Confirm Selection";
+            edit_box_confirm_btn_area.appendChild(btn_edit_box_confirm);
+
             console.log('Event selected:', selection);
             console.log(getArrayTypeEventsObjects(selection.value));
 
-            let selectorRacers = new CustomSelector("selector2", racersData, {
-                label: 'Select a racer:',
-                placeholder: 'Choose a racer of your liking',
-                onSelectionChange: function(selection) {
-                    console.log('Racer selected:', selection);
-                }
-            })
+            selector2.innerHTML = ""; // Limpiar el selector2 antes de llenarlo
+            selector3.innerHTML = ""; // Limpiar el selector3 antes de llenarlo
+            selector4.innerHTML = ""; // Limpiar el selector4 antes de llenarlo
+            selector5.innerHTML = ""; // Limpiar el selector5 antes de llenarlo
 
-            let selectorTypesEvents = new CustomSelector("selector3", getArrayTypeEventsObjects(selection.value), {
-                label: 'Select the type of event:',
-                placeholder: 'Choose a type of event of your liking',
-                onSelectionChange: function(selection) {
-                    console.log('Type of event selected:', selection);
-                }
-            });
+            switch (selection.value) {
+                case 'racer-event':
+                    racerEventSelectors(selection.value);
+                    break;
+                case 'racer-event-racer':
+                    racerEventRacerSelector(selection.value);
+                    break;
+                case 'team-event':
+                    teamEventSelectors(selection.value);
+                    break;
+                case 'lap-or-turn-event':
+                    lapOrTurnEventSelector(selection.value);
+                    break;
+                case 'amount-event':
+                    amountEventSelector(selection.value);
+                    break;
+                case 'random-event':
+                    randomEventSelector(selection.value);
+                    break;
+                default:
+                    console.log('Default event selected');
+            }
+
         }
     });
 });
+
+function racerEventSelectors(event_key){
+    
+    let racer = null
+    let event = null
+    let position = ""
+
+    let selectorRacers = new CustomSelector("selector2", racersData, {
+        label: 'Select a racer:',
+        placeholder: 'Choose a racer of your liking',
+        onSelectionChange: function(selection) {
+            console.log('Racer selected:', selection);
+            racer = selection.text; // Guardar el valor del corredor seleccionado
+        }
+    })
+
+    let selectorTypesEvents = new CustomSelector("selector3", getArrayTypeEventsObjects(event_key), {
+        
+        label: 'Select the type of event:',
+        placeholder: 'Choose a type of event of your liking',
+        onSelectionChange: function(selection) {
+            
+            selector5.innerHTML = ""
+            position = "" // Reiniciar la posición al seleccionar un nuevo evento
+
+            console.log('Type of event selected:', selection);
+            event = selection.text; // Guardar el valor del evento seleccionado
+
+            if (selection.value === 6){
+
+                let positionData = []
+
+                for (let i = 1; i < 20; i++) {
+                    positionData.push({
+                        value: i + 1,
+                        text: i + 1
+                    });
+                }
+
+                let selectorPositions = new CustomSelector("selector5", positionData, {
+                    label: 'Select the position:',
+                    placeholder: 'Choose a position of your liking',
+                    onSelectionChange: function(selection) {
+                        console.log('Position selected:', selection);
+                        position = selection.text; // Guardar el valor de la posición seleccionada
+                    }
+                });
+            }
+        }
+    });
+
+    function sendSelectedData() {
+
+        if (racer && event) {
+            
+            if (event === "P" && position === "") {
+                console.log("Please select a position for the racer.");
+                return;
+            }
+            else{
+                //btn_edit_box_confirm.removeEventListener("click", sendSelectedData); // Eliminar el evento anterior para evitar duplicados
+                console.log(racer+" "+event+position)
+            }
+        }
+        else {
+            console.log("Please select a racer and an event type.")
+        }
+    }
+
+    btn_edit_box_confirm.addEventListener("click", function(e){
+        e.preventDefault() // Prevenir el comportamiento por defecto del botón
+
+        sendSelectedData(); // Llamar a la función para enviar los datos seleccionados
+    })
+}
+
+function racerEventRacerSelector(event_key) {
+
+    let racer1 = null
+    let event = null
+    let racer2 = null
+
+    let selectorRacers1 = new CustomSelector("selector2", racersData, {
+        label: 'Select the first racer:',
+        placeholder: 'Choose a racer of your liking',
+        onSelectionChange: function(selection) {
+            console.log('Racer selected:', selection);
+            racer1 = selection.text; // Guardar el valor del primer corredor seleccionado
+        }
+    });
+
+    let selectorTypesEvents = new CustomSelector("selector3", getArrayTypeEventsObjects(event_key), {
+        label: 'Select the type of event:',
+        placeholder: 'Choose a type of event of your liking',
+        onSelectionChange: function(selection) {
+            console.log('Type of event selected:', selection);
+            event = selection.text; // Guardar el valor del evento seleccionado
+        }
+    });
+
+    let selectorRacers2 = new CustomSelector("selector4", racersData, {
+        label: 'Select the second racer:',
+        placeholder: 'Choose a racer of your liking',
+        onSelectionChange: function(selection) {
+            console.log('Racer selected:', selection);
+            racer2 = selection.text; // Guardar el valor del segundo corredor seleccionado
+        }
+    });
+
+
+    function sendSelectedData() {
+        if (racer1 && event && racer2) {
+            console.log(racer1 + " vs " + racer2 + " in " + event);
+        } else {
+            console.log("Please select both racers and an event type.");
+        }
+    }
+
+    btn_edit_box_confirm.addEventListener("click", function(e) {
+        e.preventDefault()
+
+        sendSelectedData();
+    });
+}
+
+function teamEventSelectors(event_key) {
+    let team = null
+    let event = null
+
+    let selectorTeams = new CustomSelector("selector2", teamsData, {
+        label: 'Select a team:',
+        placeholder: 'Choose a team of your liking',
+        onSelectionChange: function(selection) {
+            console.log('Team selected:', selection);
+            team = selection.text; // Guardar el valor del equipo seleccionado
+        }
+    });
+
+    let selectorTypesEvents = new CustomSelector("selector3", getArrayTypeEventsObjects(event_key), {
+        label: 'Select the type of event:',
+        placeholder: 'Choose a type of event of your liking',
+        onSelectionChange: function(selection) {
+            console.log('Type of event selected:', selection);
+            event = selection.text; // Guardar el valor del evento seleccionado
+        }
+    });
+
+    btn_edit_box_confirm.addEventListener("click", function(e) {
+        e.preventDefault()
+
+        if (team && event) {
+            console.log(team + " " + event);
+        } else {
+            console.log("Please select a team and an event type.");
+        }
+    });
+}
+
+function lapOrTurnEventSelector(event_key){
+    
+    let type = null
+    let type_value = null
+    let event = null
+
+    let specialTypeData = [
+        { value: 'Lap', text: 'Lap' },
+        { value: 'Turn', text: 'Turn' }
+    ]
+
+    let selectorSpecialType = new CustomSelector("selector2", specialTypeData, {
+        label: 'Select LAP or TURN:',
+        placeholder: 'Choose LAP or TURN',
+        onSelectionChange: function(selection) {
+            console.log('Selected:', selection);
+            type = selection.text; // Guardar el valor del corredor seleccionado
+            
+            let circuit = getNextStage()
+            console.log("Circuit selected:", circuit);
+
+            if (selection.value === 'Lap') {
+                
+                type = "Lap"
+                
+                let lapData = []
+
+                for (let i = 0; i < circuit.laps; i++) {
+                    lapData.push({
+                        value: i + 1,
+                        text: i + 1
+                    });
+                }
+
+                let selectorLaps = new CustomSelector("selector3", lapData, {
+                    label: 'Select a lap:',
+                    placeholder: 'Choose a lap of your liking',
+                    onSelectionChange: function(selection) {
+                        console.log('Lap selected:', selection);
+                        // Aquí puedes agregar lógica específica para la vuelta seleccionada
+                        type_value = selection.text; // Guardar el valor de la vuelta seleccionada
+                    }
+                });
+
+                let selectorTypesEvents = new CustomSelector("selector4", getArrayTypeEventsObjects(event_key), {
+                    label: 'Select the type of event:',
+                    placeholder: 'Choose a type of event of your liking',
+                    onSelectionChange: function(selection) {
+                        console.log('Type of event selected:', selection);
+                        event = selection.text; // Guardar el valor del evento seleccionado
+                    }
+                }); 
+            }
+            else if (selection.value === 'Turn') {
+                
+                type = "Turn"
+                let turnData = []
+
+                for (let i = 0; i < circuit.turns; i++) {
+                    turnData.push({
+                        value: i + 1,
+                        text: i + 1
+                    });
+                }
+
+                let selectorTurns = new CustomSelector("selector3", turnData, {
+                    label: 'Select a turn:',
+                    placeholder: 'Choose a turn of your liking',
+                    onSelectionChange: function(selection) {
+                        console.log('Turn selected:', selection);
+                        // Aquí puedes agregar lógica específica para la curva seleccionada
+                        type_value = selection.text; // Guardar el valor de la curva seleccionada
+                    }
+                });
+
+                let selectorTypesEvents = new CustomSelector("selector4", getArrayTypeEventsObjects(event_key), {
+                    label: 'Select the type of event:',
+                    placeholder: 'Choose a type of event of your liking',
+                    onSelectionChange: function(selection) {
+                        console.log('Type of event selected:', selection);
+                        event = selection.text; // Guardar el valor del evento seleccionado
+                    }
+                });
+            }
+        
+        }
+    })
+
+    function sendSelectedData() {
+        if (type && type_value && event) {
+            console.log(type + " " + type_value + " " + event);
+        } else {
+            console.log("Please select a type, a value and an event type.");
+        }
+    }
+
+    btn_edit_box_confirm.addEventListener("click", function(e) {
+        e.preventDefault()
+
+        sendSelectedData();
+    });
+}
+
+function amountEventSelector(event_key) {
+    let amount = null
+    let event = null
+
+    let amountData = []
+
+    for (let i = 0; i < 50; i++) {
+        amountData.push({
+            value: i + 1,
+            text: i + 1
+        });
+    }
+
+    let selectorAmount = new CustomSelector("selector2", amountData, {
+        label: 'Select an amount:',
+        placeholder: 'Choose an amount of your liking',
+        onSelectionChange: function(selection) {
+            console.log('Amount selected:', selection);
+            amount = selection.text; // Guardar el valor de la cantidad seleccionada
+        }
+    });
+
+    let selectorTypesEvents = new CustomSelector("selector3", getArrayTypeEventsObjects(event_key), {
+        label: 'Select the type of event:',
+        placeholder: 'Choose a type of event of your liking',
+        onSelectionChange: function(selection) {
+            console.log('Type of event selected:', selection);
+            event = selection.text; // Guardar el valor del evento seleccionado
+        }
+    });
+
+    function sendSelectedData() {
+        if (amount && event) {
+            console.log(amount + " " + event);
+        } else {
+            console.log("Please select an amount and an event type.");
+        }
+    }
+    
+    btn_edit_box_confirm.addEventListener("click", function(e) {
+        e.preventDefault()
+
+        sendSelectedData();
+    });
+}
+
+function randomEventSelector(event_key) {
+    let event = null
+
+    let selectorTypesEvents = new CustomSelector("selector2", getArrayTypeEventsObjects(event_key), {
+        label: 'Select the type of event:',
+        placeholder: 'Choose a type of event of your liking',
+        onSelectionChange: function(selection) {
+            console.log('Type of event selected:', selection);
+            event = selection.text; // Guardar el valor del evento seleccionado
+        }
+    });
+
+    function sendSelectedData() {
+        if (event) {
+            console.log( event);
+        } else {
+            console.log("Please select an event.");
+        }
+    }
+
+    btn_edit_box_confirm.addEventListener("click", function(e) {
+        e.preventDefault()
+
+        sendSelectedData();
+    });
+}
